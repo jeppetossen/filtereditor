6ac9cc3d1835b763273995ce95a693fb82507125
Jesper J <joppen97@live.se>
2018-10-07 20:15:45 +0200

    Refactored module-folder

--- editor/views.py
+++ editor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from editor.content import headers
+from editor.modules.content import headers
 
 
 # Create your views here.
