93da862b0e734877d30b718c13ccaeaed52d2c44
joppe <joppen97@hotmail.com>
2018-10-09 14:52:26 +0200

    Reworked find_section

--- editor/modules/filterformat/translator.py
+++ editor/modules/filterformat/translator.py
@@ -36,19 +36,36 @@ class Translator(object):
             f.close()
         return line.replace("\n", "")
 
-    def find_section(self, section):
-        sections = self.find_table_section()
-        for i in range(len(sections)):
-            if sections[i] == section:
-                pass
+    def find_section(self, part):
+        sections = list()
+        section_line = self.find_table_section(part)
         with open(self.filter, "r") as f:
             for i, line in enumerate(f):
-                print(line)
+                sections.append([i, line])
             f.close()
+        section = list()
+        for i, line in sections:
+            # print(i, section)
+            if i - 1 and line == "#" + "="*111:
+                section.insert(0, line)
+            # if line == section_line + "\n":
+            if line == section_line + "\n":
+                section.insert(1, line)
+            if i + 1 and line == "#" + "="*111:
+                section.insert(2, line)
+        del section[-1]
+        if section:
+            return section
 
-    def find_block(self):
-        # block = self.define_block()
-        with open(self.filter, "r") as f:
-            for i, line in f:
-                if line == "#":
-                    pass
+    def find_block(self, part):
+        pass
+
+    def showhide_in_part(self):
+        pass
+
+
+if __name__ == '__main__':
+    obj = Translator("filter.filter")
+    # obj.find_section("# [[0100]] OVERRIDE AREA 1 - Override ALL rules here")
+    obj.find_section("0100")
+    #print(obj.find_table_section("0100"))
