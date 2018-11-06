93e0208d77e5623e09a8b1da84b7b922b175dde4
joppe <joppen97@hotmail.com>
2018-11-06 14:05:42 +0100

    Showhide now completed

--- editor/modules/filterformat/translator.py
+++ editor/modules/filterformat/translator.py
@@ -18,7 +18,7 @@ class Translator:
             for i, line in enumerate(f):
                 pass
             f.close()
-            return i + 1
+        return i + 1
 
     def find_table_section(self, part):
         with open(self.filter, "r") as f:
@@ -48,7 +48,6 @@ class Translator:
             # print(i, section)
             if i - 1 and line == "#" + "="*111:
                 section.insert(0, line)
-            # if line == section_line + "\n":
             if line == section_line + "\n":
                 section.insert(1, line)
             if i + 1 and line == "#" + "="*111:
@@ -57,14 +56,37 @@ class Translator:
         return section
 
     def find_block(self, part):
-        pass
+        blocks = list()
+        block_line = self.find_table_block(part)
+        with open(self.filter, "r") as f:
+            for i, line in enumerate(f):
+                blocks.append([i, line])
+            f.close()
+        block = list()
+        for i, line in blocks:
+            if i - 1 and line == "#":
+                pass
+            if line == block_line + "\n":
+                pass
+            if i + 1 and line == "#":
+                pass
+        del block[-1]
+        return block
 
-    def showhide_in_part(self):
-        pass
+    def find_showhide(self):
+        with open(self.filter, "r") as f:
+            for i, line in enumerate(f):
+                if line == "Show":
+                    pass
+                elif line == "Hide":
+                    pass
+                else:
+                    return None
+                return line
 
 
 if __name__ == '__main__':
     obj = Translator("filter.filter")
     # obj.find_section("# [[0100]] OVERRIDE AREA 1 - Override ALL rules here")
     obj.find_section("0100")
-    #print(obj.find_table_section("0100"))
+    # print(obj.find_table_section("0100"))
