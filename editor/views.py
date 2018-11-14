b93685b64a20ce416163964cd25b5efd8c491038
joppe <joppen97@hotmail.com>
2018-11-14 13:02:00 +0100

    Added template for inserting data

--- editor/views.py
+++ editor/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.shortcuts import get_object_or_404
 
 from editor.modules.content import headers
 from .forms import ItemSettingsForm
@@ -10,7 +9,18 @@ from .models import ItemSettings
 # Create your views here.
 def index(request):
     if (request.method == "post") and ("sound" in request.POST):
-        pass
+        sound = request.POST.get('sound', '')
+        # skapa
+        new_item_settings = ItemSettings()
+        new_item_settings.sound = sound
+        new_item_settings.block = "block here"
+        new_item_settings.save()
+        # editera
+        item_settings = ItemSettings.objects.get(pk=settings_id)
+        item_settings.sound = sound
+        item_settings.block = "editerad block"
+        item_settings.save()
+
     else:
         sections = headers.fetch_header()
         return render(request, "editor/index.html", {"sections": sections})
