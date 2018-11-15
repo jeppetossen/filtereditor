b483c8d7b8dcf20372ddaa71a7cd9c717618c346
joppe <joppen97@hotmail.com>
2018-11-15 14:34:24 +0100

    Added placeholders for all VE-buttons

--- editor/views.py
+++ editor/views.py
@@ -2,25 +2,37 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from editor.modules.content import headers
-from .forms import ItemSettingsForm
 from .models import ItemSettings
 
 
 # Create your views here.
 def index(request):
-    if (request.method == "post") and ("sound" in request.POST):
+    if (request.method == "post") and ("show" in request.POST):
+        pass
+    elif (request.method == "post") and ("hide" in request.POST):
+        pass
+    elif (request.method == "post") and ("copy" in request.POST):
+        pass
+    elif (request.method == "post") and ("paste" in request.POST):
+        pass
+    elif (request.method == "post") and ("reset" in request.POST):
+        pass
+    elif (request.method == "post") and ("sound" in request.POST):
         sound = request.POST.get('sound', '')
         # skapa
         new_item_settings = ItemSettings()
         new_item_settings.sound = sound
-        new_item_settings.block = "block here"
+        # new_item_settings.block = "block here"
         new_item_settings.save()
         # editera
-        item_settings = ItemSettings.objects.get(pk=settings_id)
+        '''item_settings = ItemSettings.objects.get(pk=settings_id)
         item_settings.sound = sound
         item_settings.block = "editerad block"
-        item_settings.save()
-
+        item_settings.save()'''
+    elif (request.method == "post") and ("icon" in request.POST):
+        pass
+    elif (request.method == "post") and ("beam" in request.POST):
+        pass
     else:
         sections = headers.fetch_header()
         return render(request, "editor/index.html", {"sections": sections})
