e7eb734fe75f4e1dcb3118ee18df3edcbd82ae14
Jesper J <joppen97@live.se>
2018-11-20 21:56:20 +0100

    Added placeholder code for iterating json

--- editor/modules/content/headers.py
+++ editor/modules/content/headers.py
@@ -2,9 +2,22 @@ import json
 
 
 def fetch_header():
-    file = "editor/modules/content/headers.json"
+    # file = "editor/modules/content/headers.json"
+    file = "headers.json"
     with open(file, "r") as f:
         data = json.load(f)
         f.close()
 
     return data
+
+
+def create_headers_table():
+    headers = fetch_header()
+    for section, header in headers.items():
+        for title, subheader in header.items():
+            for block, item in subheader.items():
+                print(item)
+
+
+if __name__ == '__main__':
+    create_headers_table()
