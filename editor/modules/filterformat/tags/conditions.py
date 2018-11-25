0433d4ebe939dc292503a02f557a5ea0882f1a3e
Jesper J <joppen97@live.se>
2018-11-25 20:53:44 +0100

    Changed all strings to literal

--- editor/modules/filterformat/tags/conditions.py
+++ editor/modules/filterformat/tags/conditions.py
@@ -1,87 +1,87 @@
 def item_level(value, operator=""):
     if operator is not None:
-        return "ItemLevel {} {}".format(operator, value)
+        return f"ItemLevel {operator} {value}"
     else:
-        return "ItemLevel {}".format(value)
+        return f"ItemLevel {value}"
 
 
 def drop_level(value, operator=""):
     if operator is not None:
-        return "ItemLevel {} {}".format(operator, value)
+        return f"ItemLevel {operator} {value}"
     else:
-        return "ItemLevel {}".format(value)
+        return f"ItemLevel {value}"
 
 
 def quality(value, operator=""):
     if operator is not None:
-        return "Quality {} {}".format(operator, value)
+        return f"Quality {operator} {value}"
     else:
-        return "Quality {} {}".format(value)
+        return f"Quality {value}"
 
 
 def rarity(value, operator=""):
-    return "Rarity {} {}".format(operator, value)
+    return f"Rarity {operator} {value}"
 
 
 def item_class(i_class=""):
-    return "Class {}".format(i_class)
+    return f"Class {i_class}"
 
 
 def base_type(basetype):
-    return "BaseType {}".format(basetype)
+    return f"BaseType {basetype}"
 
 
 def sockets(value, operator=""):
-    return "Sockets {} {}".format(operator, value)
+    return f"Sockets {operator} {value}"
 
 
 def linked_sockets(value, operator=""):
-    return "LinkedSockets {} {}".format(operator, value)
+    return f"LinkedSockets {operator} {value}"
 
 
 def socket_group(group=""):
-    return "SocketGroup {}".format(group)
+    return f"SocketGroup {group}"
 
 
 def height(value, operator=""):
-    return "Height {} {}".format(operator, value)
+    return f"Height {operator} {value}"
 
 
 def width(value, operator=""):
-    return "Width {} {}".format(operator, value)
+    return f"Width {operator} {value}"
 
 
 def has_explicit_mod(value):
-    return "HasExplicitMod".format(value)
+    return f"HasExplicitMod {value}"
 
 
 def stack_size(value, operator=""):
-    return "StackSize {} {}".format(operator, value)
+    return f"StackSize {operator} {value}"
 
 
 def gem_level(value, operator=""):
-    return "GemLevel {} {}".format(operator, value)
+    return f"GemLevel {operator} {value}"
 
 
 def identified(boolean):
-    return "Identified {}".format(boolean)
+    return f"Identified {boolean}"
 
 
 def corrupted(boolean):
-    return "Corrupted {}".format(boolean)
+    return f"Corrupted {boolean}"
 
 
 def elder_item(boolean):
-    return "ElderItem {}".format(boolean)
+    return f"ElderItem {boolean}"
 
 
 def shaper_item(boolean):
-    return "ShaperItem {}".format(boolean)
+    return f"ShaperItem {boolean}"
 
 
 def shaped_map(boolean):
-    return "ShapedMap {}".format(boolean)
+    return f"ShapedMap {boolean}"
 
 
 def map_tier(value, operator=""):
-    return "MapTier {} {}".format(operator, value)
+    return f"MapTier {operator} {value}"
