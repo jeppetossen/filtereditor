22072f2c1e4a6b5cb2de87b181e07fd242d991e9
Jesper J <joppen97@live.se>
2018-11-27 22:12:14 +0100

    Writing to db is now working (still WIP)

--- editor/views.py
+++ editor/views.py
@@ -9,42 +9,43 @@ from .forms import ItemSettingsForm
 
 # Create your views here.
 def index(request):
-    header_keys = headers.retrieve_header_id()
-    shows = list()
-    for i in header_keys:
-        shows.append(f"show{i}")
-    for show in shows:
-        continue
-    if (request.method == "POST") and (show in request.POST):
-        print(show)
-        print(request.POST)
-        show = ItemSettings(id=1, show_hide="show")
+    # header_keys = headers.retrieve_header_id()
+    name = str()
+    number = str()
+    if request.method == "POST":
+        for k in request.POST:
+            name = k
+
+        for i in name:
+            if i.isdigit():
+                number += str(i)
+
+    if (request.method == "POST") and (f"show{number}" in request.POST):
+        show = ItemSettings(id=number, show_hide="show")
         show.save()
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and ("hide" in request.method):
+    elif (request.method == "POST") and (f"hide{number}" in request.method):
+        hide = ItemSettings(id=number, show_hide="hide")
+        hide.save()
+        return HttpResponseRedirect('')
+    elif (request.method == "POST") and (f"copy{number}" in request.method):
         pass
-    elif (request.method == "POST") and ("copy" in request.method):
+    elif (request.method == "POST") and (f"paste{number}" in request.method):
         pass
-    elif (request.method == "POST") and ("paste" in request.method):
+    elif (request.method == "POST") and (f"reset{number}" in request.method):
         pass
-    elif (request.method == "POST") and ("reset" in request.method):
-        pass
-    elif (request.method == "POST") and ("sound" in request.method):
-        sound = request.POST.get('sound', '')
-        # skapa
-        new_item_settings = ItemSettings()
-        new_item_settings.sound = sound
-        # new_item_settings.block = "block here"
-        new_item_settings.save()
-        # editera
-        '''item_settings = ItemSettings.objects.get(pk=settings_id)
-        item_settings.sound = sound
-        item_settings.block = "editerad block"
-        item_settings.save()'''
-    elif (request.method == "POST") and ("icon" in request.method):
-        pass
-    elif (request.method == "POST") and ("beam" in request.method):
-        pass
+    elif (request.method == "POST") and (f"sound{number}" in request.method):
+        show = ItemSettings(id=number, sound="")
+        show.save()
+        return HttpResponseRedirect('')
+    elif (request.method == "POST") and (f"icon{number}" in request.method):
+        show = ItemSettings(id=number, icon="")
+        show.save()
+        return HttpResponseRedirect('')
+    elif (request.method == "POST") and (f"beam{number}" in request.method):
+        show = ItemSettings(id=number, beam="")
+        show.save()
+        return HttpResponseRedirect('')
     else:
         sections = headers.fetch_header()
         return render(request, "editor/index.html", {"headers": sections})
