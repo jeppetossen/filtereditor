ca544d1de52236be8cbb80b1a0c2847e39057fe0
joppe <joppen97@live.se>
2018-12-03 20:18:40 +0100

    Improved json

--- editor/modules/headers/headers.py
+++ editor/modules/headers/headers.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_headers():
-    file = "editor/modules/headers/headers.json"
+    file = "editor/modules/headers/sections.json"
     # file = "headers.json"
     with open(file, "r") as f:
         data = json.load(f)
@@ -11,53 +11,22 @@ def load_headers():
     return data
 
 
-def retrieve_header_id():
-    headings = load_headers()
-    sections = dict()
-    subsections = dict()
-    blocks = dict()
-    tier_keys = dict()
+def get_sections():
+    data = load_headers()
+    sections = list()
+    for item in data['Sections']:
+        for section in data['Sections'][item]:
+            if section == "name":
+                sections.append(data['Sections'][item][section])
 
-    i = 0
-    k = 0
-    v = 0
-    n = 0
-    for section, headers in headings.items():
-        i += 1
-        sections[section] = i
-        for subsection, tiers in headers.items():
-            k += 1
-            subsections[subsection] = i
-            for tier, items in tiers.items():
-                v += 1
-                blocks[tier] = i
-                for x, z in items.items():
-                    try:
-                        n += 1
-                        tier_keys[x] = i
-                    except KeyError:
-                        return "Failed to retrieve id."
-    return f"Sections: {sections}\nSubsections: {subsections}\nBlocks: {blocks}\nID's: {tier_keys}"
-
-    # return sections, subsections, blocks, tier_keys
+    return sections
 
 
-def get_subsection_id():
-    headings = load_headers()
-    names = list()
-    numbers = list()
-
-    for section, section_items in headings.items():
-        for subsection, subsection_items in section_items.items():
-            names.append(section)
-
-    for i in range(1, len(names)+1):
-        numbers.append(i)
-
-    return numbers
+def get_subsections():
+    pass
 
 
 if __name__ == '__main__':
-    print(get_subsection_id())
+    print(get_sections())
     # print(retrieve_header_id())
     # print(str(len(retrieve_header_id()[1])))
