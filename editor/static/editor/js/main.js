7f36632d8738271e8a647af76545ccded1c25851
joppe <joppen97@hotmail.com>
2018-12-20 13:53:39 +0100

    Added open/close for subsection dropdown

--- editor/static/editor/js/main.js
+++ editor/static/editor/js/main.js
@@ -1,12 +1,4 @@
 /*$(document).ready(function () {
-    $(".group-header").on('click', function (event) {
-        let target_parent = $(event.target).parent().find(".container-visual-content");
-        if (target_parent.css("display") === "none") {
-            target_parent.css("display", "flex");
-        } else {
-            target_parent.css("display", "none");
-        }
-    });
 
     $(".ps-colorpicker").spectrum({
         preferredFormat: "rgb",
@@ -24,19 +16,23 @@ $(document).ready(function () {
     $(".Subsection").on('click', function (event) {
         let element = event.target;
         let elementClist = element.classList;
-        targetIdNumber(element.id);
-        //alert(targetId(event));
+        let elementIdNumber = element.id.split("_")[1];
+        let subsectionContent = targetElementById(elementIdNumber).classList;
         if (elementClist.contains("closed")) {
+            subsectionContent.remove("closed");
+            subsectionContent.add("open");
             elementClist.remove("closed");
             elementClist.add("open");
         } else if (elementClist.contains("open")) {
+            subsectionContent.remove("open");
+            subsectionContent.add("closed");
             elementClist.remove("open");
             elementClist.add("closed");
         }
     });
 
     function targetId(element) {
-        return element.target.id;
+        return element.id;
     }
 
     function targetIdNumber(elementId) {
@@ -44,13 +40,18 @@ $(document).ready(function () {
         return elementSplit[1];
     }
 
-    function toggleSubsectionBlocks(parent) {
-        if (parent.contains("closed")) {
-            parent.remove("closed");
-            parent.add("open");
-        } else if (parent.contains("open")) {
-            parent.remove("open");
-            parent.add("closed");
+    function targetElementById(elementId) {
+        let element = document.getElementById("SubsectionBlockContent_" + elementId);
+        return element;
+    }
+
+    function toggleSubsectionBlocks(element) {
+        if (element.contains("closed")) {
+            element.remove("closed");
+            element.add("open");
+        } else if (element.contains("open")) {
+            element.remove("open");
+            element.add("closed");
         }
     }
 });
