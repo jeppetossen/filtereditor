41eb442099143cd6d2516baba810b192eb5fbc21
joppe <joppen97@live.se>
2019-01-22 14:05:03 +0100

    Testing heroku

--- filtereditor/settings.py
+++ filtereditor/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'filtereditor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
+        'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
         'HOST': '',
