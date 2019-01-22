40fb297840e908a3ae92b0ac9752c00b38bd9f4b
joppe <joppen97@live.se>
2019-01-22 14:06:21 +0100

    Testing heroku

--- filtereditor/settings.py
+++ filtereditor/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': '',
         'PORT': '',
     },
