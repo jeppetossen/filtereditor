53348d3f1fc98909d7d9d6c2c43e5ab832b0a5a0
joppe <joppen97@live.se>
2019-01-22 13:21:20 +0100

    Removed ttd from setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='filtereditor',
     version='0.1.2',
-    packages=['editor', 'editor.ttd', 'editor.items', 'editor.headers', 'editor.migrations', 'editor.templatetags',
+    packages=['editor', 'editor.items', 'editor.headers', 'editor.migrations', 'editor.templatetags',
               'filtereditor'],
     url='',
     license='',
