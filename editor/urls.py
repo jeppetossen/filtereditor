4755d34c56e7080c85ffafadefe7939f1dfe6992
joppe <joppen97@live.se>
2019-02-11 14:43:57 +0100

    Removed redundant views

--- editor/urls.py
+++ editor/urls.py
@@ -6,7 +6,4 @@ from . import views
 app_name = 'editor'
 urlpatterns = [
     path('', views.index, name="home"),
-    path('theme/', views.ThemePageView.as_view(), name="theme"),
-    path('settings/', views.SettingsPageView.as_view(), name="settings"),
-    path('about/', views.AboutPageView.as_view(), name="about"),
 ]
--- editor/views.py
+++ editor/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 
 from editor.modules.headers import headers
@@ -21,32 +20,36 @@ def index(request):
             if i.isdigit():
                 number += str(i)
 
-    if (request.method == "POST") and (f"show{number}" in request.POST):
+    if (request.method == "POST") and (f"show_{number}" in request.POST):
         show = models.ItemSettings(id=number, show_hide="show")
         show.save()
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and (f"hide{number}" in request.POST):
+    elif (request.method == "POST") and (f"hide_{number}" in request.POST):
         hide = models.ItemSettings(id=number, show_hide="hide")
         hide.save()
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and (f"copy{number}" in request.POST):
+    elif (request.method == "POST") and (f"disable_{number}" in request.POST):
+        disable = models.ItemSettings(id=number, show_hide="disable")
+        disable.save()
+        return HttpResponseRedirect('')
+    elif (request.method == "POST") and (f"copy_{number}" in request.POST):
         pass
-    elif (request.method == "POST") and (f"paste{number}" in request.POST):
+    elif (request.method == "POST") and (f"paste_{number}" in request.POST):
         pass
-    elif (request.method == "POST") and (f"reset{number}" in request.POST):
+    elif (request.method == "POST") and (f"reset_{number}" in request.POST):
         settings.reset(number)
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and (f"sound{number}" in request.POST):
+    elif (request.method == "POST") and (f"sound_{number}" in request.POST):
         sound = models.PlayAlertSound(id=number, sound_id=int(), volume=int())
         sound_apply = models.ItemSettings(id=number, sound=sound)
         sound_apply.save()
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and (f"icon{number}" in request.POST):
+    elif (request.method == "POST") and (f"icon_{number}" in request.POST):
         icon = models.MinimapIcon(id=number, size=int(), color="", shape="")
         icon_apply = models.ItemSettings(id=number, icon=icon)
         icon_apply.save()
         return HttpResponseRedirect('')
-    elif (request.method == "POST") and (f"beam{number}" in request.POST):
+    elif (request.method == "POST") and (f"beam_{number}" in request.POST):
         beam = models.PlayEffect(id=number, color="", temp="")
         beam_apply = models.ItemSettings(id=number, beam=beam)
         beam_apply.save()
@@ -54,15 +57,3 @@ def index(request):
     else:
         sections = headers.load_headers()
         return render(request, "editor/index.html", {"sections": sections})
-
-
-class ThemePageView(TemplateView):
-    template_name = "editor/theme.html"
-
-
-class SettingsPageView(TemplateView):
-    template_name = "editor/settings.html"
-
-
-class AboutPageView(TemplateView):
-    template_name = "editor/about.html"
