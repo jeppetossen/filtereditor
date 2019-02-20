f11089093d6a34b345408736d3b7cad58aea3e01
joppe <joppen97@live.se>
2019-02-20 13:44:58 +0100

    All strings converted to literal strings

--- editor/modules/filterformat/tags/actions.py
+++ editor/modules/filterformat/tags/actions.py
@@ -1,40 +1,40 @@
 def set_border_color(red, green, blue, alpha=None):
     if alpha is not None:
-        return "SetBorderColor {} {} {} {}".format(red, green, blue, alpha)
+        return f"SetBorderColor {red} {green} {blue} {alpha}"
     else:
-        return "SetBorderColor {} {} {}".format(red, green, blue)
+        return f"SetBorderColor {red} {green} {blue}"
 
 
 def set_text_color(red, green, blue, alpha=None):
     if alpha is not None:
-        return "SetTextColor {} {} {} {}".format(red, green, blue, alpha)
+        return f"SetTextColor {red} {green} {blue} {alpha}"
     else:
-        return "SetTextColor {} {} {}".format(red, green, blue)
+        return f"SetTextColor {red} {green} {blue}"
 
 
 def set_background_color(red, green, blue, alpha=None):
     if alpha is not None:
-        return "SetBackgroundColor {} {} {} {}".format(red, green, blue, alpha)
+        return f"SetBackgroundColor {red} {green} {blue} {alpha}"
     else:
-        return "SetBackgroundColor {} {} {}".format(red, green, blue)
+        return f"SetBackgroundColor {red} {green} {blue}"
 
 
 def set_font_size(font_size=32):
-    return "SetFontSize {}".format(font_size)
+    return f"SetFontSize {font_size}"
 
 
 def play_alert_sound(sid, volume=None):
     if volume is not None:
-        return "PlayAlertSound {} {}".format(sid, volume)
+        return f"PlayAlertSound {sid} {volume}"
     else:
-        return "PlayAlertSound {}".format(sid)
+        return f"PlayAlertSound {sid}"
 
 
 def play_alert_sound_positional(sid, volume=None):
     if volume is not None:
-        return "PlayAlertSoundPositional {} {}".format(sid, volume)
+        return f"PlayAlertSoundPositional {sid} {volume}"
     else:
-        return "PlayAlertSound {}".format(sid)
+        return f"PlayAlertSound {sid}"
 
 
 def disable_drop_sound():
@@ -42,12 +42,12 @@ def disable_drop_sound():
 
 
 def custom_alert_sound(file=""):
-    return "CustomAlertSound {}".format(file)
+    return f"CustomAlertSound {file}"
 
 
 def minimap_icon(size, color, shape):
-    return "MinimapIcon {} {} {}".format(size, color, shape)
+    return f"MinimapIcon {size} {color} {shape}"
 
 
 def play_effect(color, temp=""):
-    return "PlayEffect {} {}".format(color, temp)
+    return f"PlayEffect {color} {temp}"
