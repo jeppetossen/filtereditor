466e0210133c117f469872a2b3af55c3df0b412f
joppe <joppen97@live.se>
2019-02-20 13:46:16 +0100

    DRF test

--- filtereditor/urls.py
+++ filtereditor/urls.py
@@ -17,7 +17,28 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from django.conf.urls import url
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     path('', include('editor.urls')),
+    path('route', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
