0569dafb610b0720cea8c0c4005a66764e54f88a
joppe <joppen97@live.se>
2019-04-11 21:28:07 +0200

    Removed redundant files views.py and forms.py

--- editor/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Block
-
-
-class BlockForm(ModelForm):
-    class Meta:
-        model = Block
-        fields = ['section', 'block', 'show_hide', 'rarity', 'sound', 'icon', 'beam']
